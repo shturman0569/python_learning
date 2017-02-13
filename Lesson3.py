5f60d3d85773134db9e7fc201dd3d88fc6c86c61
shturman0569 <bdorovskyi@gmail.com>
2017-02-13 21:25:33 +0200

    Lessson

---
+++ Lesson3.py
@@ -0,0 +1,20 @@
+# # import sys
+# # import math
+# #
+# # # x = sys.argv[1]
+# # # y = int(sys.argv[2])
+# # # r = x * y
+# # # print(r)
+# # print(sys.argv)
+# # x = float(sys.argv[1])
+# #
+# # r = math.sqrt(x)
+# # print(r)
+#
+# import math
+#
+# x = int(input("enter number:"))
+#
+#
+# res = math.sin(x)**2 + math.cos(x)**2
+# print(res)
---
+++ Lessson3.1.py
@@ -0,0 +1,24 @@
+# s = input('Enter string')
+# if s == 'Hi':
+#     print ('Hello')
+# print('Goodbay')
+
+# x = int(input ("Enter number: "))
+# if 0 < x <=100 :
+#     print("Correct number")
+# else:
+#     if x < 0:
+#         print(" I don't want a negative number")
+
+
+# x = int(input ("Enter number: "))
+# if 0 < x <=100 :
+#     print("Correct number")
+# elif x < 0:
+#     print(" I don't want a negative number")
+# else:
+#     print("Number >100")
+
+while True:
+    print("Type 'quit' to exit")
+    phrase = input("Your massage")
